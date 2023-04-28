a8643092ba30d39798a67aab4950c5a6b9c0c834
HuyLam <tranlamhuy5tn@gmail.com>
2023-04-28 21:06:30 +0700

    update dburi

--- BE/app/config/index.js
+++ BE/app/config/index.js
@@ -3,7 +3,7 @@ const config = {
     port: process.env.PORT || 5050,
   },
   db: {
-    uri: process.env.MONGODB_URI || "mongodb+srv://B2017044:EGXa8Mwc@cluster0.aiiqg4k.mongodb.net/Movies",
+    uri: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Movies",
   },
   JWT_SECRET: "" + process.env.JWT_SECRET
   
